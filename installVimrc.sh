2eeb9a80515a707f157932fdd9cfd2e629afd12e
flotisable <s09930698@gmail.com>
2017-12-17 13:55:29 +0800

    move rc files location variables to a seperate file

--- installVimrc.sh
+++ installVimrc.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-vimrcDir=~
-nvimrcDir=~/.config/nvim
-
-vimrcSourceFile=".vimrc"
-nvimrcSourceFile="init.vim"
-
-vimrcTargetFile=".vimrc"
-nvimrcTargetFile="init.vim"
-
-if [ $# -eq 1 -a -n $1 ]; then vimrcDir=$1;         fi
-if [ $# -eq 2 -a -n $2 ]; then vimrcTargetFile=$2;  fi
-if [ $# -eq 3 -a -n $3 ]; then nvimrcDir=$3;        fi
-if [ $# -eq 4 -a -n $4 ]; then nvimrcTargetFile=$4; fi
-if [ $# -eq 5 -a -n $5 ]; then vimrcSourceFile=$5;  fi
-if [ $# -eq 6 -a -n $6 ]; then nvimrcSourceFile=$6; fi
+source ./settings
 
 cp ${vimrcSourceFile} ${vimrcDir}/${vimrcTargetFile}
 cp ${nvimrcSourceFile} ${nvimrcDir}/${nvimrcTargetFile}
--- makefile
+++ makefile
@@ -1,17 +1,10 @@
-vimrcDir=$(HOME)
-nvimrcDir=$(HOME)/.config/nvim
-
-vimrcSourceFile=.vimrc
-nvimrcSourceFile=init.vim
-
-vimrcTargetFile=.vimrc
-nvimrcTargetFile=init.vim
+include settings
 
 all: $(vimrcDir)/$(vimrcTargetFile) $(nvimrcDir)/$(nvimrcTargetFile)
 	cp $^ .
 
 install:
-	./installVimrc.sh $(vimrcDir) $(vimrcTargetFile) $(nvimrcDir) $(nvimrcTargetFile) $(vimrcSourceFile) $(nvimrcSourceFile)
+	./installVimrc.sh
 
 uninstall:
 	rm $(vimrcDir)/$(vimrcTargetFile)
