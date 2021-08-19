334f5a1e2ba1c751747ccf18c09f11b5b556b3f0
flotisable <s09930698@gmail.com>
2021-08-19 22:16:51 +0800

    update install script to use new setting file

--- install.sh
+++ install.sh
@@ -1,26 +1,46 @@
 #!/bin/sh
+settingFile="./settings.toml"
 
-if [ -z ${OS} ]; then
-  OS=$(uname -s);
-fi
+. ./readSettings.sh ${settingFile}
 
-echo "detected OS: ${OS}"
+installFile()
+{
+  local sourceFile=$1
+  local targetFile=$2
+  local fileMessage=$3
 
-. ./settings
+  echo "install $fileMessage"
+  cp $sourceFile $targetFile 
+}
 
-if [ -z ${nvimrcDir} ]; then
-  nvimrcDir=$(./default.sh ${OS});
-fi
+targetTableName=$(mapFind "settings" "target")
+sourceTableName=$(mapFind "settings" "source")
+dirTableName=$(mapFind "settings" "dir")
+pluginManagerTableName=$(mapFind "settings" "pluginManager")
 
-if [ \( ${installPluginManager} -eq 1 \) -a ! -e ${pluginManagerPath}/plug.vim ]; then
+if [ "$(mapFind "$pluginManagerTableName" "install")" == "1" -a ! -e $(mapFind "$pluginManagerTableName" "path")/plug.vim ]; then
   echo "install vim-plug";
-  curl -fLo ${pluginManagerPath}/plug.vim --create-dirs \
+  curl -fLo $(mapFind "$pluginManagerTableName" "path")/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
 fi
 
-echo "install vimrc"
-cp ${vimrcSourceFile} ${vimrcDir}/${vimrcTargetFile}
-echo "install nvim init file"
-cp ${nvimrcSourceFile} ${nvimrcDir}/${nvimrcTargetFile}
-echo "install nvim ginit file"
-cp ${ngvimrcSourceFile} ${nvimrcDir}/${ngvimrcTargetFile}
+for target in $(mapKeys "$targetTableName"); do
+
+  targetFile=$(mapFind "$targetTableName" "$target")
+  sourceFile=$(mapFind "$sourceTableName" "$target")
+
+  if [ "$target" == "vimrc" ]; then
+
+    dirType="vim"
+
+  else
+
+    dirType="nvim"
+
+  fi
+
+  dir=$(mapFind "$dirTableName" "$dirType")
+
+  installFile $sourceFile $dir/$targetFile $target
+
+done
